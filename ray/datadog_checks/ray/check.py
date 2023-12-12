22146744daea8d4ff67ae49963a4d861a5e879cf
Florent Clarret <florent.clarret@datadoghq.com>
2023-12-12 08:07:05 +0100

    gpu

--- ray/datadog_checks/ray/check.py
+++ ray/datadog_checks/ray/check.py
@@ -12,6 +12,16 @@ class RayCheck(OpenMetricsBaseCheckV2, ConfigMixin):
     __NAMESPACE__ = 'ray'
     DEFAULT_METRIC_LIMIT = 0
 
+    def __init__(self, name, init_config, instances=None):
+        super().__init__(name, init_config, instances)
+        self.bytes = 100
+
+    def check(self, instance):
+        super().check(instance)
+        self.gauge('node.gpus_utilization', range(0, 100))
+        self.gauge('node.gram_used', self.bytes)
+        self.bytes += 200
+
     def get_default_config(self):
         return {
             "metrics": [METRIC_MAP],
