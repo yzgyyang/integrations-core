82e12e88f52d270035a0b25206003dd263010822
Florent Clarret <florent.clarret@datadoghq.com>
2024-03-19 15:59:36 +0100

    simplify

--- ddev/tests/cli/env/test_stop.py
+++ ddev/tests/cli/env/test_stop.py
@@ -52,14 +52,11 @@ def test_stop_all(ddev, helpers, data_dir, mocker):
 
     running_env = [('postgres', 'py3.12'), ('sqlserver', 'py3.8'), ('cockroachdb', 'py3.7')]
 
-    integration = 'all'
-    environment = 'all'
-
     for intg, env in running_env:
         env_data = EnvDataStorage(data_dir).get(intg, env)
         env_data.write_metadata({})
 
-    result = ddev('env', 'stop', integration, environment)
+    result = ddev('env', 'stop', 'all', 'all')
 
     assert result.exit_code == 0, result.output
     assert result.output == helpers.dedent(
